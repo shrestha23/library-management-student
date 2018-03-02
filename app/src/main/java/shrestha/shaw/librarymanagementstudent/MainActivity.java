048df490cf270e83cd2cb1c913f2703e72e894ed
shrestha23 <30778713+shrestha23@users.noreply.github.com>
2018-03-02 18:10:10 +0530

    created skeleton activities

--- app/src/main/java/shrestha/shaw/librarymanagementstudent/MainActivity.java
+++ app/src/main/java/shrestha/shaw/librarymanagementstudent/MainActivity.java
@@ -1,13 +1,29 @@
 package shrestha.shaw.librarymanagementstudent;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText librarianIdField;
+    ImageButton submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitButton = findViewById(R.id.search_btn);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
